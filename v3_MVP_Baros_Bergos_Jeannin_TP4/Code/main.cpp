f6916d26eb3867d13a76be3a969be97dbe654961
brs64 <106591433+brs64@users.noreply.github.com>
2024-05-24 09:22:32 +0200

    Add files via upload

--- v3_MVP_Baros_Bergos_Jeannin_TP4/Code/main.cpp
+++ v3_MVP_Baros_Bergos_Jeannin_TP4/Code/main.cpp
@@ -2,7 +2,8 @@
 #include "presentation.h"
 #include "modele.h"
 #include <QApplication>
-
+#include <iostream>
+using namespace std;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
--- v3_MVP_Baros_Bergos_Jeannin_TP4/Code/sousProgrammes.cpp
+++ v3_MVP_Baros_Bergos_Jeannin_TP4/Code/sousProgrammes.cpp
@@ -112,20 +112,20 @@ void charger(Diaporamas& pDiaporamas)
 }
 void charger (Images& pImages) {
     Image imageACharger;
-    imageACharger = Image("objet", "", "C:\\cartesDisney\\Disney_tapis.gif");
+    imageACharger = Image("objet", "", "..\\cartesDisney\\Disney_tapis.gif");
     pImages.push_back(imageACharger);
-    imageACharger = Image("personnage", "Blanche Neige", "C:\\cartesDisney\\Disney_4.gif");
+    imageACharger = Image("personnage", "Blanche Neige", "..\\cartesDisney\\Disney_4.gif");
     pImages.push_back(imageACharger);
-    imageACharger = Image("personnage", "Alice", "C:\\cartesDisney\\Disney_2.gif");
+    imageACharger = Image("personnage", "Alice", "..\\cartesDisney\\Disney_2.gif");
     pImages.push_back(imageACharger);
-    imageACharger = Image("animal", "Mickey", "C:\\cartesDisney\\Disney_19.gif");
+    imageACharger = Image("animal", "Mickey", "..\\cartesDisney\\Disney_19.gif");
     pImages.push_back(imageACharger);
-    imageACharger = Image("personnage", "Pinnochio", "C:\\cartesDisney\\Disney_29.gif");
+    imageACharger = Image("personnage", "Pinnochio", "..\\cartesDisney\\Disney_29.gif");
     pImages.push_back(imageACharger);
-    imageACharger = Image("objet", "chateau", "C:\\cartesDisney\\Disney_0.gif");
+    imageACharger = Image("objet", "chateau", "..\\cartesDisney\\Disney_0.gif");
     pImages.push_back(imageACharger);
-    imageACharger = Image("personnage", "Minnie", "C:\\cartesDisney\\Disney_14.gif");
+    imageACharger = Image("personnage", "Minnie", "..\\cartesDisney\\Disney_14.gif");
     pImages.push_back(imageACharger);
-    imageACharger = Image("animal", "Bambi", "C:\\cartesDisney\\Disney_3.gif");
+    imageACharger = Image("animal", "Bambi", "..\\cartesDisney\\Disney_3.gif");
     pImages.push_back(imageACharger);
 }
