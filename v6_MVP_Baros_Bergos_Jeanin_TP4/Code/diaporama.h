07002d5cdc08727af6a6ce236abf14a77c022f0c
Guillem J <153082806+Guigou64@users.noreply.github.com>
2024-06-07 09:25:49 +0200

    Add files via upload

---
+++ v6_MVP_Baros_Bergos_Jeanin_TP4/Code/diaporama.h
@@ -0,0 +1,55 @@
+#ifndef DIAPORAMA_H
+#define DIAPORAMA_H
+#include <vector>
+#include "imageDansDiaporama.h"
+#include "database.h"
+
+
+typedef vector<ImageDansDiaporama*> ImagesDiaporama; // collection de pointeurs vers les images de ce
+    // diaporama (= pteurs vers objets de type ImageDansDiaporama)
+class Diaporama
+{
+
+    private:
+        // Attributs
+        unsigned int id;                // identifiant du diaporama dans la Base de données
+        string titre ;                  // titre du diaporama
+        unsigned int vitesseDefilement; // vitesse de défilement des images du diaporama
+        unsigned int posImageCourante;  // position de l'image courante dans le diaporama
+        ImagesDiaporama images;         // vecteur de pointeurs sur les objets ImageDansDiaporama de ce diaporama
+        database db;
+        // Fonction.s/procédure.s
+        void trierParRangCroissant(); // trie les images du diaporama par ordre de rang croissant
+
+
+    public:
+        Diaporama(); // Constructeur
+        ~Diaporama(); // Destructeur
+
+        // Getters
+        unsigned int getId() const;
+        string getTitre() const;
+        int getVitesseDefilement() const;
+        ImagesDiaporama getImages() const;
+        ImageDansDiaporama* getImageCourante() const; // retourne l'image courante du diaporama
+        unsigned int getPosImageCourante() const; // retourne la position de l'image courante dans le diaporama
+
+        unsigned int nbImages() const;  // nbre de pointeurs d'images contenus dans diaporama
+
+
+        // Setters
+        void setId(unsigned int pId);
+        void setTitre();
+        void setVitesseDefilement();
+        void setImages(const ImagesDiaporama& pImages);
+
+
+        // Autres
+        void ajouterImageEnFin(ImageDansDiaporama* pImage);     // ajoute une image au diaporama (en fin de l'attribut images)
+        void enleverImageEnFin();   // enlève la dernière image du diaporama, et delete l'objet image enlevé
+        void vider();       // enlève toutes les images du diaporama, et delete chaque objet enlevé
+        void charger();     // chargement, dans l'attribut images, des images associées au diaporama courant (d'attribut id).
+
+};
+
+#endif // DIAPORAMA_H
---
+++ v6_MVP_Baros_Bergos_Jeanin_TP4/Code/presentation.cpp
@@ -0,0 +1,162 @@
+#include "presentation.h"
+#include "diaporama.h"
+Presentation::Presentation()
+{
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(demanderAvancer()));
+}
+
+Modele *Presentation::getModele()
+{
+    return _leModele;
+}
+
+lecteurVue *Presentation::getVue()
+{
+    return _laVue;
+}
+
+void Presentation::setModel(Modele *m)
+{
+    _leModele = m;
+}
+
+void Presentation::setVue(lecteurVue *v)
+{
+    _laVue = v;
+}
+
+ImageDansDiaporama* Presentation::getImageActuelle()
+{
+    return _leModele->getDiaporama()->getImageCourante();
+}
+
+Diaporama *Presentation::getDiapoActuel()
+{
+    return getModele()->getDiaporama();
+}
+
+
+void Presentation::demanderAvancer() {
+    qDebug() << "essaie d'avancer";
+    if (!_leModele) {
+        qDebug() << "_leModele est nul";
+        return;
+    }
+    if (!_laVue) {
+        qDebug() << "_laVue est nul";
+        return;
+    }
+
+    _leModele->avancer();
+/*
+    if(getModele()->getManuel())
+    {
+        demanderChangerMode();
+    }
+*/
+    Diaporama *diapo = _leModele->getLecteur()->getDiaporama();
+    if (!diapo) {
+        qDebug() << "Diaporama est nul";
+        return;
+    }
+    ImageDansDiaporama* image = _leModele->getLecteur()->getImageCourante();
+    _laVue->majPresentation(
+        QString::fromStdString(diapo->getTitre()),
+        QString::fromStdString(image->getTitre()),
+        QString::fromStdString(image->getCategorie()),
+        QString::number(image->getRangDansDiaporama()),
+        QString::fromStdString(image->getChemin())
+        );
+    qDebug() << "L'image avance";
+}
+
+void Presentation::demanderReculer() {
+    if (!_leModele) {
+        qDebug() << "_leModele est nul";
+        return;
+    }
+    if (!_laVue) {
+        qDebug() << "_laVue est nul";
+        return;
+    }
+
+    _leModele->reculer();
+
+    Diaporama *diapo = _leModele->getLecteur()->getDiaporama();
+    if (!diapo) {
+        qDebug() << "Diaporama est nul";
+        return;
+    }
+
+    if(getModele()->getManuel())
+    {
+        demanderChangerMode();
+    }
+
+    ImageDansDiaporama* image = _leModele->getLecteur()->getImageCourante();
+    _laVue->majPresentation(
+        QString::fromStdString(diapo->getTitre()),
+        QString::fromStdString(image->getTitre()),
+        QString::fromStdString(image->getCategorie()),
+        QString::number(image->getRangDansDiaporama()),
+        QString::fromStdString(image->getChemin())
+        );
+    qDebug() << "L'image recule";
+}
+
+void Presentation::demanderPause()
+{
+    qDebug() << "la lecture est en pause !";
+}
+
+
+void Presentation::demanderChangerVitesse()
+{
+
+    _leModele->changerVitesse();
+    qDebug() << "La fenêtre pour changer la vitesse apparait";
+}
+
+void Presentation::demanderChangerMode()
+{
+    Diaporama *diapo = _leModele->getLecteur()->getDiaporama();
+    unsigned int temps = DELAIS * (diapo->getVitesseDefilement());
+    qDebug() << "La vitesse de defilement : " << temps;
+    QString modeDef;
+    qDebug() << "Le mode change, il était à: " << getModele()->getManuel();
+    if (getModele()->getManuel()) {
+        getModele() -> setManuel(false);
+        qDebug() << "auto";
+        modeDef = "Auto";
+        timer->start(temps);
+        qDebug() << "chrono lancé. actuellement à : " << timer;
+        qDebug() << "chrono continue. actuellement à : " << timer;
+    }
+    else {
+        getModele() -> setManuel(true);
+        modeDef = "Manuel";
+        timer->stop();  // Arrêter le timer lorsque le mode est changé en manuel.
+        disconnect(timer, SIGNAL(timeout()), this, SLOT(avancer()));
+        }
+    getVue()->afficherMode(modeDef);
+}
+
+
+void Presentation::demanderChargerDiapo()
+{
+    _leModele->chargerDiapo();
+    qDebug() << "On charges une nouvelle diapo";
+}
+
+void Presentation::demanderFiltre()
+{
+    qDebug() << "Affichage des filtres possibles";
+}
+
+void Presentation::passerManuel(){
+    if (!getModele()->getManuel()){
+        demanderChangerMode();
+    }
+}
+
